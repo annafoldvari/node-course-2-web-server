5b85b28f6c2947caeba663a9f8d2c5218845cac3
anna_f <annafoldvari@gmx.co.uk>
2018-05-04 16:28:28 +0100

    Maintenance middleware removed.Projects template added.

--- server.js
+++ server.js
@@ -21,9 +21,6 @@ app.use((req, res, next) => {
    next();
 });
 
-app.use((req, res, next) => {
-   res.render('maintenance.hbs');
-});
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
@@ -42,13 +39,19 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
   });
 });
 
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: "There was an error."
+    });
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects', {
+        pageTitle: 'Projects Page'
     });
 })
 
